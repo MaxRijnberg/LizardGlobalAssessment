025cf9710f45242c4ea56e649d3f64cfbcacc4c4
Max Rijnberg <0966485@hr.nl>
2023-07-20 17:29:36 +0200

    buggy localhost

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+import data from "C:\\Users\\maxri\\LizardGlobalAssessment\\src\\mock\\data.json"
+import React from "react";
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  return <div>
+    <head>
+      <title>Lorem ipsum dolor library</title>
+    </head>
+  <h1>Below, you can see all the books in the library.</h1>
+  <p>There are {data.length} books in the library.</p>
+
+  // Here I will make full list of books / posts
+  // This is the main page
+  <p>{data["posts"]}</p>
+
+  // On the left side of the screen, I intend to make a menu with filters
+  // You can filter by year, author and tags
+
+  </div>;
 }
 
-export default App;
+export default App();
